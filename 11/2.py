30606587592d2b40347c3374b107a1a85542bb21
Andrew Rueckert <ruke47@users.noreply.github.com>
2021-12-10 21:24:59 -0800

    Day 11

---
+++ 11/2.py
@@ -0,0 +1,58 @@
+class Octopus:
+    def __init__(self, energy):
+        self.energy = energy
+        self.flashed_this_turn = False
+
+    def tick(self):
+        self.energy += 1
+        if self.energy > 9:
+            self.flashed_this_turn = True
+        return self.flashed_this_turn
+
+    def receive_energy(self):
+        self.energy += 1
+        if self.energy > 9 and not self.flashed_this_turn:
+            self.flashed_this_turn = True
+            return True
+        else:
+            return False
+
+    def simmer_down(self):
+        if self.flashed_this_turn:
+            self.energy = 0
+            self.flashed_this_turn = False
+
+grid = []
+with open("input.txt") as file:
+    for line in file:
+        grid.append([Octopus(int(x)) for x in line.strip()])
+
+for turn in range(1000000000):
+    has_flashed = set()
+    to_flash = set()
+    for x, row in enumerate(grid):
+        for y, octopus in enumerate(row):
+            if octopus.tick():
+                to_flash.add((x,y))
+
+    while len(to_flash) > 0:
+        x,y = to_flash.pop()
+        has_flashed.add((x,y))
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                tx = x + dx
+                ty = y + dy
+                if tx < 0 or ty < 0 or tx > 9 or ty > 9:
+                    continue
+                if grid[tx][ty].receive_energy():
+                    if (tx, ty) not in has_flashed:
+                        to_flash.add((tx, ty))
+
+    turn_flashers = len(has_flashed)
+    if turn_flashers == 100:
+        print(f"Frabjous day! On turn {turn+1} everyone flashed!")
+        break
+
+    for row in grid:
+        for octopus in row:
+            octopus.simmer_down()
